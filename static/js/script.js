b8b3011fddbb7412184d755351cfffdfb6c1eca6
Jonnycp <jonathan-caputo@hotmail.com>
2022-10-13 23:06:01 +0200

    Aggiungi / togli animazioni
    setDisabledMenu disabilita i link quando non vengono mostrati.. forse inutile

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+let hamburger = document.querySelector("nav .menuToggle input[type='checkbox']");
+hamburger.addEventListener("change", (e) => {
+    let ul = document.querySelector("nav ul");
+    if(hamburger.checked){
+        ul.dataset.animation = "enter";
+        setDisabledMenu(true);
+    }else{
+        ul.dataset.animation = "exit";
+        setDisabledMenu();
+    }
+});
+
+const setDisabledMenu = (del=false) => {
+    const disable = (e) => {
+        e.preventDefault();
+    }
+    
+    let menu = document.querySelectorAll("nav ul a");
+    menu.forEach((item) => {
+        if(del){
+            item.removeEventListener("click", disable);
+        }else{
+            item.addEventListener("click", disable);
+        }
+    });
+}
